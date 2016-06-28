b0630787a2abe4f4164f9cc7e45f8d50400cb6b1
stefanovnm <stefanov.nm@gmail.com>
2016-06-28 15:39:12 +0300

    4 - 100% 10 - 30%

--- 04.Arrays/04.MaximalIncreasingSequence.js
+++ 04.Arrays/04.MaximalIncreasingSequence.js
@@ -1,30 +1,18 @@
 function solve(args) {
     var lines = args[0].split('\n').map(Number),
+        n = lines[0],
         count = 1,
-        countGlobal = 1,
-        isEqual = 0;
+        countGlobal = 1;
 
-    for (var i = 1; i < lines[0]; i++) {
-        if ((parseInt(lines[i]) + 1) == parseInt(lines[i + 1])) {
-            if (isEqual == 1) {
-                count += 1;
-                if (count > countGlobal) {
-                    countGlobal = count;
-                }
-            } else {
-                isEqual = 1;
-                count += 1;
-                if (count > countGlobal) {
-                    countGlobal = count;
-                }
-            }
+    for (var i = 1; i < n; i+=1) {
+        if (parseInt(lines[i]) < +parseInt(lines[i + 1])) {
+            count += 1;
         } else {
-            isEqual = 0;
+            if (count > countGlobal) {
+                countGlobal = count;
+            }
             count = 1;
         }
     }
     console.log(countGlobal);
 }
-//TODO
-//First test is ok and then 0/100
-//something is wrong here and cannot find what?
--- 04.Arrays/10.PrimeNumbers.js
+++ 04.Arrays/10.PrimeNumbers.js
@@ -1,18 +1,45 @@
 function solve(args) {
-    //probably wants to return the biggest prime number before the input number?
     var toCheck = parseInt(args[0]);
-//    console.log(args[0]);
 
-    for (var i = toCheck; i > 0; i--) {
-        if (isPrime(i)) {
-            console.log(i);
-            break;
+    function eratosthenes(n) {
+        var array = [],
+            upperLimit = Math.sqrt(n),
+            output;
+
+        for (var i = 0; i <= n; i++) {
+            array.push(true);
         }
-    }
 
-    //this is real shit... regex to find prime :) http://www.noulakaz.net/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
-    function isPrime(n) {
-        return !(Array(n + 1).join(1).match(/^1?$|^(11+?)\1+$/));
-    }
+        for (var i = 2; i <= upperLimit; i++) {
+            if (array[i]) {
+                for (var j = i * i; j <= n; j += i) {
+                    array[j] = false;
+                }
+            }
+        }
+
+        for (var i = n; i >= 2; i--) {
+            if (array[i]) {
+                output = i;
+                break;
+            }
+        }
+
+        return output;
+    };
+
+    var result = eratosthenes(toCheck);
+    console.log(result);
 }
-//gives only 20point but without any task this is good
+
+//this is real shit... regex to find prime :) http://www.noulakaz.net/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
+/*
+function isPrime(n) {
+    return !(Array(n + 1).join(1).match(/^1?$|^(11+?)\1+$/));
+};
+*/
+
+
+//http://stackoverflow.com/questions/15471291/sieve-of-eratosthenes-algorithm-in-javascript-running-endless-for-large-number
+
+
